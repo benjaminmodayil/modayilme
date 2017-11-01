20457042c796cf5766ed8dffb5f7adc2738c126b
Benjamin Modayil <modayilme@gmail.com>
2017-11-01 05:56:50 -0400

    svgfix,crossed fingers

--- src/templates/workPreview.js
+++ src/templates/workPreview.js
@@ -5,59 +5,66 @@ import Updates from '../_includes/Updates'
 import IconKeurig from '../assets/work-images/IconKeurig'
 import * as invert from '../utils/invertColor'
 
-export default function Template({ data }) {
-  const { markdownRemark: post } = data
-
-  let note
-  if (post.frontmatter.imageNote) {
-    note = <p dangerouslySetInnerHTML={{ __html: post.frontmatter.imageNote }} />
+class Template extends React.Component {
+  svgRender() {
+    if (this.props.data.markdownRemark.frontmatter.svgName == IconKeurig.name) {
+      return true
+    }
   }
 
-  let svgAlias
-  let value
-  if (post.frontmatter.svgName) {
-    svgAlias = post.frontmatter.svgName
-  }
-  if (IconKeurig.name == svgAlias) {
-    value = <IconKeurig />
+  componentDidMount() {
+    // console.log(this.svgRender())
+    this.forceUpdate()
   }
 
-  return (
-    <main className="work__preview-page">
-      <article className="work">
-        <header
-          className="work__header"
-          style={{ backgroundColor: post.frontmatter.themeColor }}
-        >
-          <div className="work__header-inner wrapper wrapper--wide margin-center">
-            <div className="work__header-inner--left container--medium-to-small ">
-              <h1 className="h-1">{post.frontmatter.title}</h1>
-              <p>{post.frontmatter.tldr}</p>
+  render() {
+    const { markdownRemark: post } = this.props.data
+
+    let note
+    if (post.frontmatter.imageNote) {
+      note = <p dangerouslySetInnerHTML={{ __html: post.frontmatter.imageNote }} />
+    }
+
+    return (
+      <main className="work__preview-page">
+        <article className="work">
+          <header
+            className="work__header"
+            style={{ backgroundColor: post.frontmatter.themeColor }}
+          >
+            <div className="work__header-inner wrapper wrapper--wide margin-center">
+              <div className="work__header-inner--left container--medium-to-small ">
+                <h1 className="h-1">{post.frontmatter.title}</h1>
+                <p>{post.frontmatter.tldr}</p>
+              </div>
+              {post.frontmatter.svgName == IconKeurig.name ? (
+                <div className="work__header-inner--right">
+                  <IconKeurig />
+                </div>
+              ) : null}
             </div>
-            <div className="work__header-inner--right">
-              {value}
-              {note}
+          </header>
+
+          <section className="work__preview__body">
+            <div className="work__preview__container wrapper wrapper--medium margin-center section-padding--bottom-heavy">
+              <div
+                className="post-work__body"
+                dangerouslySetInnerHTML={{ __html: post.html }}
+              />
+              <small className="post__signature col__left">Ben M.</small>
             </div>
-          </div>
-        </header>
+          </section>
 
-        <section className="work__preview__body">
-          <div className="work__preview__container wrapper wrapper--medium margin-center section-padding--bottom-heavy">
-            <div
-              className="post-work__body"
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
-            <small className="post__signature col__left">Ben M.</small>
+          <div className="post__misc">
+            <Updates />
           </div>
-        </section>
-
-        <div className="post__misc">
-          <Updates />
-        </div>
-      </article>
-    </main>
-  )
+        </article>
+      </main>
+    )
+  }
 }
+
+export default Template
 
 export const workQuery = graphql`
   query WorkStudyByPath($path: String!) {
