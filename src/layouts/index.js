dc5b31dcf63fd30a5d28c87d1719c821aaa1cb52
Benjamin Modayil <modayilme@gmail.com>
2017-11-10 13:15:23 -0500

    wip

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -14,9 +14,36 @@ class TemplateWrapper extends React.Component {
     }
   }
 
+  themify = () => {
+    // Yank theme color from localStorage and use it.
+    document.documentElement.style.setProperty('--red', 'blue')
+
+    // // document.documentElement.style.setProperty(
+    // //   '--mainColor',
+    // //   localStorage.getItem('userThemeColor')
+    // // )
+
+    // localStorage.setItem('userThemeColor', this.value)
+
+    // var colorInput = document.querySelector('#choose-theme-color')
+
+    // colorInput.addEventListener('change', function() {
+    //   // Theme the site!
+    //   document.documentElement.style.setProperty('--mainColor', this.value)
+
+    //   // Save the value for next time page is visited.
+    //   document.documentElement.style.setProperty(
+    //     '--mainColor',
+    //     localStorage.getItem('userThemeColor')
+    //   )
+    //   localStorage.setItem('userThemeColor', this.value)
+    // })
+  }
+
   render() {
     return (
       <div className="site__container">
+        {this.themify()}
         <Helmet
           title="Welcome to modayil.me"
           meta={[
