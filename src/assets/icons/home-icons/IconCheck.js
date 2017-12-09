9d24db365f438f25002394ea81aed096935768a8
Benjamin Modayil <modayilme@gmail.com>
2017-12-09 14:15:14 -0500

    emoji conversion to icons

---
+++ src/assets/icons/home-icons/IconCheck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function IconCheck(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={31} height={31} viewBox="0 0 31 31" {...props}>
+      <g fill="none" transform="translate(.83 .57)">
+        <path fill="#72C472" d="M28.6956522,30 L1.30434783,30 C0.583976087,30 0,29.4160239 0,28.6956522 L0,1.30434783 C0,0.583976087 0.583976087,0 1.30434783,0 L28.6956522,0 C29.4160239,0 30,0.583976087 30,1.30434783 L30,28.6956522 C30,29.4160239 29.4160239,30 28.6956522,30 Z" />
+        <polygon fill="#FFF" points="12.391 22.062 5.329 15 7.174 13.155 12.391 18.373 22.826 7.938 24.671 9.783" />
+      </g>
+    </svg>
+  );
+}
---
+++ src/assets/icons/home-icons/IconSkills.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function IconSkills(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={36} height={31} viewBox="0 0 36 31" {...props}>
+      <g fill="none" transform="translate(.583 .14)">
+        <path fill="#444" d="M35.3846154,5.38461538 L35.3846154,1.53846154 C35.3846154,0.688461538 34.6961538,0 33.8461538,0 L1.53846154,0 C0.688461538,0 0,0.688461538 0,1.53846154 L0,5.38461538 L35.3846154,5.38461538 Z" />
+        <path fill="#606060" d="M33.8461538,30 L10.7692308,30 L10.7692308,5.38461538 L35.3846154,5.38461538 L35.3846154,28.4615385 C35.3846154,29.3115385 34.6961538,30 33.8461538,30 Z" />
+        <circle cx="2.692" cy="2.692" r="1.154" fill="#E86C60" />
+        <circle cx="6.538" cy="2.692" r="1.154" fill="#EFD358" />
+        <circle cx="10.385" cy="2.692" r="1.154" fill="#72C472" />
+        <path fill="#4F4F4F" d="M11.5384615,30 L1.53846154,30 C0.688461538,30 0,29.3115385 0,28.4615385 L0,5.38461538 L11.5384615,5.38461538 L11.5384615,30 Z" />
+        <rect width="5.385" height="1.538" x="3.077" y="7.692" fill="#B3B3B3" />
+        <rect width="3.846" height="1.538" x="3.077" y="12.308" fill="#B3B3B3" />
+        <rect width="5.385" height="1.538" x="3.077" y="16.923" fill="#B3B3B3" />
+        <rect width="3.846" height="1.538" x="3.077" y="21.538" fill="#B3B3B3" />
+        <rect width="5.385" height="1.538" x="3.077" y="26.154" fill="#B3B3B3" />
+        <rect width="3.846" height="1.538" x="14.615" y="7.692" fill="#E86C60" />
+        <rect width="5.385" height="1.538" x="17.692" y="12.308" fill="#EFD358" />
+        <rect width="7.692" height="1.538" x={20} y="16.923" fill="#FFF" />
+        <rect width="5.385" height="1.538" x="17.692" y="21.538" fill="#EFD358" />
+        <rect width="3.846" height="1.538" x="14.615" y="26.154" fill="#E86C60" />
+      </g>
+    </svg>
+  );
+}
