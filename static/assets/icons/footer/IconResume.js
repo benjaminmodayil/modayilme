ec606f6f1c8f597a6f948b271b103ec49c426af3
Benjamin Modayil <modayilme@gmail.com>
2018-06-27 23:29:35 -0400

    new icons

---
+++ static/assets/icons/footer/IconResume.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function IconResume(props) {
+  return (
+    <svg width="23px" height="26px" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="Welcome" fill="none" fillRule="evenodd">
+        <g id="Desktop-HD-Copy-17" transform="translate(-953 -508)" stroke="#FFF" strokeWidth={2}>
+          <g id="icon-resume" transform="translate(954 509)">
+            <path id="Shape" d="M14.4 0v6.4h6.4" />
+            <path id="Shape" strokeLinecap="square" d="M14.4 0H0v24h20.8V6.4z" />
+            <path d="M4.8 16.8H16" id="Shape" strokeLinecap="square" />
+            <path d="M4.8 12H16" id="Shape" strokeLinecap="square" />
+            <path d="M4.8 7.2h4.8" id="Shape" strokeLinecap="square" />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
