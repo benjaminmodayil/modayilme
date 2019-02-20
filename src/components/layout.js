123cc5a2cb3733bb62b2bd5dfdd5c68b6c8c6562
Benjamin Modayil <modayilme@gmail.com>
2019-02-20 13:55:44 -0500

    testing google font

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React, { useState } from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Link from 'gatsby-link'
+// import Header from "./header"
 
-import Header from "./header"
-import "./layout.css"
+import 'normalize.css'
+import './layout.css'
 
-const Layout = ({ children }) => (
+require('typeface-permanent-marker')
+require('typeface-lato')
+
+const Layout = props => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -18,29 +22,28 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+        {/*// <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <nav>
+          <Link to="/">
+            <acronym title="Benjamin Modayil" class="font-special text-red text-3xl">
+              B.M.
+            </acronym>
+          </Link>
+
+          <div>
+            <Link to="/">blog</Link>
+            <Link to="/">work log</Link>
+            <Link to="/">ui challenge</Link>
+            <Link to="/">contact</Link>
+          </div>
+        </nav>
+        <div>
+          <main>{props.children}</main>
+          <footer>© {new Date().getFullYear()}</footer>
         </div>
       </>
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
