77b6c247ecd2a9b460bf0555b7af7be653d2351e
Benjamin Modayil <modayilme@gmail.com>
2020-02-24 23:29:41 -0500

    progress

---
+++ src/components/ServiceSummary.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Link from './Link';
+import useMedia from '../hooks/useMedia';
+
+export default function ServiceSummary({
+  onTap,
+  isActiveCheck,
+  initialPosY,
+  finalPosY,
+  initialPosX,
+  finalPosX,
+  Icon,
+  iconRow,
+  title,
+  description,
+  url,
+  image,
+  siteName,
+  workDescription
+}) {
+  // { x: isActiveCheck ? finalPosX : initialPosX },
+  // { y: isActiveCheck ? finalPosY : initialPosX }
+  const size = useMedia(
+    // 640px
+    // 1024px
+    ['(min-width: 1024px)', '(min-width: 0)'],
+    ['lg', 'sm']
+    // { y: isActiveCheck ? finalPosY : initialPosX }
+  );
+  console.log(size);
+  return (
+    <motion.div
+      className={`bg-white shadow-md rounded p-4 lg-p-8 max-w-lg lg-max-w-2xl w-full absolute ${
+        isActiveCheck ? 'z-10' : 'z-0'
+      }`}
+      initial={{ opacity: 0.7, x: initialPosX, y: initialPosY }}
+      animate={{
+        // x: isActiveCheck ? initialPosX : finalPosX,
+        // ...directionType,
+        x: isActiveCheck && size === 'lg' ? finalPosX : size !== 'lg' ? 0 : initialPosX,
+        y: isActiveCheck && size === 'sm' ? finalPosY : size !== 'sm' ? 0 : initialPosY,
+        opacity: isActiveCheck ? 1 : 0.7,
+        scale: isActiveCheck ? 1 : 0.5
+      }}
+      onTap={onTap}
+    >
+      <h3 className="flex justify-center items-center text-2xl leading-none text-lg font-semibold font-sans text-purple-600 -ml-12">
+        <Icon
+          width="52"
+          height="52"
+          className="mr-2 stroke-current group-hover-text-white"
+        />
+        {title}
+      </h3>
+      <div className="mt-6 flex justify-between w-48 mx-auto">
+        {iconRow.map((Icon, index) => {
+          return <Icon key={index} width="60" height="60" />;
+        })}
+      </div>
+      <p className="mt-4 leading-normal max-w-lg mx-auto text-base lg-text-lg">
+        {description}
+      </p>
+      <Link
+        isExternal="true"
+        to={url}
+        className="bg-white rounded flex block shadow-md max-w-lg mx-auto mt-12"
+      >
+        <span
+          style={{
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundImage: `url(${image})`,
+            width: '240px',
+            height: 'auto'
+          }}
+        />
+        <div className="p-4">
+          <h4 className="font-semibold mb-2">{siteName}</h4>
+          <p className="mb-2 text-sm italic leading-normal">{workDescription}</p>
+
+          <span className="text-sm text-purple-600 font-semibold self-end hover-underline">
+            Visit
+          </span>
+        </div>
+      </Link>
+    </motion.div>
+  );
+}
--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import WorkPreviewCard from '../WorkPreviewCard';
+import { motion } from 'framer-motion';
 import Link from '../Link';
+import ServiceSummary from '../ServiceSummary';
 import IconDesign from '../../images/IconDesign';
 import IconDevelopment from '../../images/IconDevelopment';
 import IconSketch from '../../images/IconSketch';
@@ -12,16 +13,18 @@ import MarvinKnorrThumbnail from '../../images/thumbnail-marvinknorrlaw.jpg';
 export default function Services() {
   const [activeTab, setActiveTab] = useState('design');
   const [isDetailOpen, setDetailOpen] = useState(false);
-  let workPreviews = [1, 2, 3];
+
   const onDetailClick = e => {
     // `current` points to the mounted text input element
     setDetailOpen(!e.currentTarget.open);
     // inputEl.current.focus();
   };
+
+  const isDesignActive = activeTab === 'design';
+  const isDevelopmentActive = activeTab === 'development';
   return (
     <section className="bg-gray-100 py-32 px-4" id="services">
       <h2 className="sr-only">Services</h2>
-
       <div className="xl-max-w-6xl mx-auto">
         <p className="text-3xl font-medium font-sans leading-normal text-center mb-8">
           I provide services in Design and Development
@@ -45,55 +48,47 @@ export default function Services() {
             Sometimes you need someone that can touch design and development.
           </p>
         </details>
-        <div className="mt-16 flex flex-col lg-flex-row justify-center">
-          <div className="bg-white shadow-md rounded p-8 max-w-2xl w-full">
-            <h3 className="flex justify-center items-center text-2xl leading-none text-lg font-semibold font-sans text-purple-600 -ml-12">
-              <IconDesign
-                width="52"
-                height="52"
-                className="mr-2 stroke-current group-hover-text-white"
-              />
-              Design
-            </h3>
-            <div className="mt-6 flex justify-between w-48 mx-auto">
-              <IconSketch width="60" height="60" />
-              <IconInvisionStudio width="60" height="60" />
-              <IconWhimsical width="60" height="60" />
-            </div>
-            <p className="mt-4 leading-normal max-w-lg mx-auto text-lg">
-              My typical process can include me researching your market competitors,
-              looking at current design trends in your industry, providing mood boards,
-              designing wireframes, and designing full artboards in sketch. All while you
-              approve and provide feedback throughout the process.
-            </p>
-            <Link
-              isExternal="true"
-              to={`https://www.marvinknorrlaw.com`}
-              className="bg-white rounded flex block shadow-md max-w-lg mx-auto mt-12"
-            >
-              <span
-                style={{
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundImage: `url(${MarvinKnorrThumbnail})`,
-                  width: '240px',
-                  height: 'auto'
-                }}
-              />
-              <div className="p-4">
-                <h4 className="font-semibold mb-2">Marvin Knorr Law</h4>
-                <p className="mb-2 text-sm italic leading-normal">
-                  Designed with Invision Studio and developed using GatsbyJS and
-                  NetlifyCMS.
-                </p>
+        <div className="mt-16 flex justify-center min-h-half">
+          <ServiceSummary
+            onTap={() => setActiveTab('design')}
+            isActiveCheck={isDesignActive}
+            initialPosX={0}
+            finalPosX="-30vw"
+            initialPosY={0}
+            finalPosY="-30vw"
+            Icon={IconDesign}
+            iconRow={[IconSketch, IconInvisionStudio, IconWhimsical]}
+            title="Design"
+            description="My typical process can include me researching your market competitors,
+      looking at current design trends in your industry, providing mood boards,
+      designing wireframes, and designing full artboards in sketch. All while you
+      approve and provide feedback throughout the process."
+            url="https://www.marvinknorrlaw.com"
+            image={MarvinKnorrThumbnail}
+            siteName="Marvin Knorr Law"
+            workDescription="Designed with Invision Studio and developed using GatsbyJS and
+      NetlifyCMS."
+          />
 
-                <span className="text-sm text-purple-600 font-semibold self-end hover-underline">
-                  Visit
-                </span>
-              </div>
-            </Link>
-          </div>
+          <ServiceSummary
+            onTap={() => setActiveTab('development')}
+            isActiveCheck={isDevelopmentActive}
+            initialPosX={0}
+            finalPosX="30vw"
+            initialPosY={0}
+            finalPosY="30vh"
+            Icon={IconDevelopment}
+            iconRow={[IconSketch, IconInvisionStudio, IconWhimsical]}
+            title="Development"
+            description="My typical process can include me researching your market competitors,
+        looking at current design trends in your industry, providing mood boards,
+        designing wireframes, and designing full artboards in sketch. All while you
+        approve and provide feedback throughout the process."
+            url="http://master--hacktopia.netlify.com"
+            image={HacktopiaThumbnail}
+            siteName="Hacktopia"
+            workDescription="Developed using GatsbyJS, TailwindCSS, and NetlifyCMS"
+          />
         </div>
       </div>
     </section>
