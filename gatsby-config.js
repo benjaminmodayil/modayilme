375b4bb8ec570765a073c9967df2059a18982edb
Benjamin Modayil <modayilme@gmail.com>
2020-03-01 20:52:26 -0500

    strip netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,8 +71,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-postcss`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
