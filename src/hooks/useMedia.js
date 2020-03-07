2d8cf66bb7648fc97dd5181f1c51d1ef2d7377c8
Benjamin Modayil <modayilme@gmail.com>
2020-03-06 19:35:28 -0500

    fix hook

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 export default function useMedia(queries, values, defaultValue) {
   // Array containing a media query list for each query
-  const mediaQueryLists = queries.map(q => window.matchMedia(q));
+  const mediaQueryLists = queries.map(q => window && window.matchMedia(q));
 
   // Function that gets value based on matching media query
   const getValue = () => {
